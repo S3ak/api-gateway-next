f7812b98e0c32720df664071394c026150da0fe1
Monde Sineke <nsineke@gmail.com>
2024-02-05 19:24:51 +0100

    test: :white_check_mark: Create GET and POST test
    create api test

--- cypress/e2e/spec-api.cy.ts
+++ cypress/e2e/spec-api.cy.ts
@@ -7,20 +7,19 @@ const ENV_URL = Cypress.env("BASE_API_URL") || "http://localhost:3000/api";
  * Test products resource, CRUD operations.
  */
 describe("GET /api/products", () => {
-  it.skip("gets a list of products", () => {
+  it("gets a list of products", () => {
     cy.request("GET", `${ENV_URL}/products`).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.message).to.eq(
-        "Success: Resource has been successfully retrieved. | Invoices"
+        "Success: Resource has been successfully retrieved. | products"
       );
       expect(response.body.data).to.have.length(5);
       // FIXME: Flakey test
-      expect(response.body.data[0].name).to.eq("Delba de Oliveira");
-      expect(response.body.data[0].amount).to.eq(8945);
-      expect(response.body.data[0].customer_id).to.eq(undefined);
-      expect(response.body.data[0].date).to.eq(undefined);
-      expect(response.body.data[0].id).to.eq(undefined);
-      expect(response.body.data[0].status).to.eq(undefined);
+      expect(response.body.data[0].name).to.eq("Gorgeous Metal Gloves");
+      expect(response.body.data[0].price).to.eq(`633.00`);
+      expect(response.body.data[0].id).to.eq(
+        "caa1a0a3-b2eb-4057-8fa0-964ab72e095a"
+      );
 
       expect(response.body.count).to.eq(5);
     });
@@ -32,27 +31,21 @@ describe("POST /api/products", () => {
   it("Create a product", () => {
     const payload = {
       name: `Shoe maker ${Math.random()}`,
-      amount: 8945,
-      customer_id: crypto.randomUUID(),
-      status: "paid",
+      price: 8945,
+      id: crypto.randomUUID(),
+      description: "A nice description",
     };
 
     cy.request("POST", `${ENV_URL}/products`, { ...payload }).then(
       (response) => {
         expect(response.status).to.eq(200);
         expect(response.body.message).to.eq(
-          "Success: Resource has been successfully created. | Invoice"
+          "Success: Resource has been successfully created. | product"
         );
         expect(response.body.data).to.have.length(1);
-        // FIXME: Flakey test
         expect(response.body.data[0].name).to.eq(payload.name);
-        expect(response.body.data[0].amount).to.eq(8945);
-        expect(response.body.data[0].customer_id).to.eq(payload.customer_id);
-        expect(response.body.data[0].date).to.eq(undefined);
-        expect(response.body.data[0].id).to.eq(payload.customer_id);
-        expect(response.body.data[0].status).to.eq(payload.status);
-
-        expect(response.body.count).to.eq(5);
+        expect(response.body.data[0].price).to.eq(`${payload.price}.00`);
+        expect(response.body.data[0].id).to.eq(payload.id);
       }
     );
   });
